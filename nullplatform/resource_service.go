9b5761f28c617b6ea0ad1875f7f9e4fb54ad78b4
Matias Navarro <matiasg.developer@gmail.com>
2024-05-08 17:16:21 -0300

    fix: delete service

--- nullplatform/resource_service.go
+++ nullplatform/resource_service.go
@@ -235,7 +235,7 @@ func ServiceDelete(d *schema.ResourceData, m any) error {
 
 	serviceID := d.Id()
 
-	err := nullOps.DeleteService(serviceID, pService)
+	err := nullOps.DeleteService(serviceID)
 	if err != nil {
 		return err
 	}
