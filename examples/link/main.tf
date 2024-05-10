3fc7ad433d1d7829ad758a7b5b23c858a87d483c
Matias Navarro <matiasg.developer@gmail.com>
2024-05-10 09:57:18 -0300

    update link and default status

--- examples/link/main.tf
+++ examples/link/main.tf
@@ -1,10 +1,30 @@
+data "nullplatform_application" "app" {
+  id = var.null_application_id
+}
+
+resource "nullplatform_service" "redis_cache_test" {
+  name             =  "redis-cache"
+  specification_id = "4a4f6955-5ae0-40dc-a1de-e15e5cf41abb"
+  entity_nrn       = data.nullplatform_application.app.nrn
+  linkable_to      = [data.nullplatform_application.app.nrn]
+  dimensions = {}
+  selectors = {
+    imported = false,
+  }
+  attributes = {}
+}
+
+data "nullplatform_service" "redis" {
+  id = nullplatform_service.redis_cache_test.id
+}
+
 resource "nullplatform_link" "link_redis" {
   name             = "link_from_terraform_2"
   status           = "active"
-  service_id       = "36432d16-ec69-48e4-b5f5-18a63e7034c1"
+  service_id       = data.nullplatform_service.redis.id
   specification_id = "66919464-05e6-4d78-bb8c-902c57881ddd"
-  entity_nrn       = "organization=1255165411:account=95118862:namespace=249561561:application=1460930848"
-  linkable_to      = ["organization=1255165411:account=95118862:namespace=249561561:application=1460930848"]
+  entity_nrn       = data.nullplatform_application.app.nrn
+  linkable_to      = [data.nullplatform_application.app.nrn]
   selectors = {
     imported = false,
   }
