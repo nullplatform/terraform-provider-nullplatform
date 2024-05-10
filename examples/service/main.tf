b98dff0c4080372abfb6266aa2cd6f23bc8bc043
Matias Navarro <matiasg.developer@gmail.com>
2024-05-10 09:44:51 -0300

    update fields

--- examples/service/main.tf
+++ examples/service/main.tf
@@ -15,16 +15,16 @@ data "nullplatform_service" "service" {
 }
 
 resource "nullplatform_service" "open_weather_test" {
-  name              = "open weather"
+  name              = "open-weather"
   specification_id  = var.specification_id
-  entity_nrn        = "organization=123456:account=12345"
-  linkable_to       = ["organization=123456:account=12345"]
-  status            = "creating"
+  entity_nrn        = "organization=1255165411:account=95118862"
+  linkable_to       = ["organization=1255165411:account=95118862"]
+  status            = "active"
   selectors = {
-    "category"      = "SaaS"
-    "imported"      = true
-    "provider"      = "OpenWeather"
-    "sub_category"  = "Weather"
+    category      = "SaaS",
+    imported      = true,
+    provider      = "OpenWeather",
+    sub_category  = "Weather",
   }
   attributes = {
     api_key = var.api_key
