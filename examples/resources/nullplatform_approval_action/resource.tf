d77dfb213a2000f4a77632c0665665691d2aa25a
Sebastian Nallar <sebastiansaidnallar@gmail.com>
2025-01-24 13:54:51 -0300

    Fix: docs and exmaple for policies

--- examples/resources/nullplatform_approval_action/resource.tf
+++ examples/resources/nullplatform_approval_action/resource.tf
@@ -1,49 +1,58 @@
 terraform {
   required_providers {
     nullplatform = {
-      source = "nullplatform/nullplatform"
+        source = "nullplatform/nullplatform"
     }
   }
 }
 
 provider "nullplatform" {}
 
-resource "nullplatform_notification_channel" "slack" {
-  nrn    = "organization=1255165411:account=95118862:namespace=1493172477:application=113444824"
-  type   = "slack"
-  source = ["approval", "service"]
-  
-  configuration {
-    slack {
-      channels = ["#alerts", "#platform-notifications"]
+resource "nullplatform_approval_action" "deployment_create" {
+    nrn = "organization=12551165411:account=2:namespace=3:application=123"
+    entity = "deployment"
+    action = "deployment:create"
+    
+    dimensions = {
+      environment = "production"
     }
-  }
+
+    on_policy_success = "approve"
+    on_policy_fail = "manual"
+
+    policies = [
+      nullplatform_approval_policy.example_policy.id
+    ]
 }
 
-resource "nullplatform_notification_channel" "webhook" {
-  nrn  = "organization=1255165411:account=95118862:namespace=1493172477:application=113444824"
-  type = "http"
-  source = ["approval", "service"]
-  
-  configuration {
-    http {
-      url = "https://hooks.example.com/webhook/xyz"
+resource "nullplatform_approval_action" "scope_delete" {
+    nrn = "organization=12551165411:account=2:namespace=3:application=123"
+    entity = "scope"
+    action = "scope:delete"
+    
+    dimensions = {
+        environment = "production"
     }
-  }
+
+    on_policy_success = "approve"
+    on_policy_fail = "manual"
+
+    policies = [
+      nullplatform_approval_policy.example_policy.id
+    ]
 }
 
-resource "nullplatform_notification_channel" "github" {
-  nrn    = "organization=1255165411:account=95118862:namespace=1493172477:application=113444824"
-  type   = "github"
-  source = ["approval", "service"]
-  
-  configuration {
-    github {
-      account         = "GalactusPlatform"
-      reference       = "main"
-      repository      = "demo-nullplatform-services-manager"
-      workflow_id     = "service-action.yml"
-      installation_id = "57594772"
+resource "nullplatform_approval_action" "scope_create" {
+    account = "test-account"
+    entity = "scope"
+    action = "scope:create"
+    dimensions = {
+      environment = "production"
     }
-  }
+    on_policy_success = "approve"
+    on_policy_fail = "manual"
+
+    policies = [
+      nullplatform_approval_policy.example_policy.id
+    ]
 }
--- examples/resources/nullplatform_approval_policy/resource.tf
+++ examples/resources/nullplatform_approval_policy/resource.tf
@@ -8,34 +8,19 @@ terraform {
 
 provider "nullplatform" {}
 
-resource "nullplatform_approval_policy" "example_with_account_name" {
-  account = "kwik-e-mart-main"
-  name    = "Example Approval Police-2"
-  conditions = jsonencode({
-    entity = "deployment"
-    action = "create"
-    rules = [
-      {
-        type     = "environment-2"
-        operator = "in"
-        values   = ["production"]
-      }
-    ]
-  })
+resource "nullplatform_approval_policy" "example" {
+ account = "test-account"
+ name   = "Code Coverage Policy - Minimum for production 80%"
+ conditions = jsonencode({
+   "build.metadata.coverage.percentage" = { "$gte" = 80 }
+ })
 }
 
 resource "nullplatform_approval_policy" "example" {
   nrn    = "organization=1255165411:account=95118862:namespace=1493172477:application=113444824"
-  name   = "Example Approval Policy"
+  name   = "Auto Scaling Policy - Min Instances 2"
   conditions = jsonencode({
-    entity = "deployment"
-    action = "create"
-    rules = [
-      {
-        type     = "environment-2"
-        operator = "in"
-        values   = ["production"]
-      }
-    ]
+    "scope.capabilities.auto_scaling.enabled" = true,
+    "scope.capabilities.auto_scaling.instances.min_amount" = 2
   })
 }
